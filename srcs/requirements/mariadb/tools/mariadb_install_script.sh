10bf92d9cb0a0478d7f17f48620dcf49c3184a26
Linux User <bguillau@kody>
2024-03-06 20:36:09 +0000

    mariadb ko pb avec openrc, surement rc-service mariadb restart qui a besoin de sudorigths

--- srcs/requirements/mariadb/tools/mariadb_install_script.sh
+++ srcs/requirements/mariadb/tools/mariadb_install_script.sh
@@ -1,6 +1,11 @@
 rc-service mariadb setup
 rc-service mariadb start
 
+SQL_DB_NAME=db
+SQL_USER_NAME=yo
+SQL_USER_PASSWORD=yo
+SQL_ROOT_PASSWORD=yop
+
 mysql -e "CREATE DATABASE IF NOT EXISTS \`${SQL_DB_NAME}\`;"
 mysql -e "CREATE USER IF NOT EXISTS \`${SQL_USER_NAME}\`@'localhost' IDENTIFIED BY '${SQL_USER_PASSWORD}';"
 mysql -e "GRANT ALL PRIVILEGES ON \`${SQL_DB_NAME}\`.* TO \`${SQL_USER_NAME}\`@'%' IDENTIFIED BY '${SQL_USER_PASSWORD}';"
