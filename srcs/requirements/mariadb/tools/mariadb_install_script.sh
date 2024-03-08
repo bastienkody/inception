4bf6b9a8613ca0d4dc02dc4187cf77febad64953
Bastien Guillaume <bguillau@student.42.fr>
2024-03-08 20:27:36 +0100

    on avance un peu plus

--- srcs/requirements/mariadb/tools/mariadb_install_script.sh
+++ srcs/requirements/mariadb/tools/mariadb_install_script.sh
@@ -4,25 +4,24 @@ if [ ! -d "/run/mysqld" ]; then
 	chown -R mysql:mysql /run/mysqld
 fi
 
-# add to boot (not needed for this project I guess)
-#rc-update add mariadb
-
+if [ ! -d "/var/lib/mysql/mysql" ]; then
+	mysql_install_db --basedir=/usr --datadir=/var/lib/mysql --user=mysql
+fi
 # set up mariadb ; seems like it creates db and 2 users
 #rc-service mariadb setup
 
-# we call this as entrypoint in dockerfile; it calls mysqld_safe
-#rc-service mariadb restart
+# launch mariadb server
+rc-service mariadb restart
 
 SQL_DB_NAME=db
-SQL_USER_NAME=yo
+SQL_USER_NAME=mysql
 SQL_USER_PASSWORD=yo
 SQL_ROOT_PASSWORD=yop
 
-mysql -e "CREATE DATABASE IF NOT EXISTS \`${SQL_DB_NAME}\`;"
-mysql -e "CREATE USER IF NOT EXISTS \`${SQL_USER_NAME}\`@'localhost' IDENTIFIED BY '${SQL_USER_PASSWORD}';"
-mysql -e "GRANT ALL PRIVILEGES ON \`${SQL_DB_NAME}\`.* TO \`${SQL_USER_NAME}\`@'%' IDENTIFIED BY '${SQL_USER_PASSWORD}';"
+mysql -u mysql -e "CREATE DATABASE IF NOT EXISTS \`${SQL_DB_NAME}\`;"
+mysql -u mysql -e "CREATE USER IF NOT EXISTS \`${SQL_USER_NAME}\`@'localhost' IDENTIFIED BY '${SQL_USER_PASSWORD}';"
+mysql -u mysql -e "GRANT ALL PRIVILEGES ON \`${SQL_DB_NAME}\`.* TO \`${SQL_USER_NAME}\`@'%' IDENTIFIED BY '${SQL_USER_PASSWORD}';"
 
-mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${SQL_ROOT_PASSWORD}';"
-mysql -e "FLUSH PRIVILEGES;"
+mysql -u mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${SQL_ROOT_PASSWORD}';"
+mysql -u mysql -e "FLUSH PRIVILEGES;"
 
-mysqladmin -u root -p${SQL_ROOT_PASSWORD} shutdown
