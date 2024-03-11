7f3dbd574ed15569e25ce010ac8c0856dfad233d
bguillau <bguillau@student.42.fr>
2024-03-11 20:21:09 +0000

    pb user'mysql' in wordpress to connect to mariadb, see link kinsta to debug

--- srcs/requirements/wordpress/tools/wordpress_creation_script.sh
+++ srcs/requirements/wordpress/tools/wordpress_creation_script.sh
@@ -2,8 +2,13 @@
 
 cd /var/www/html/wordpress
 
-#only if wp not installed
+# for mariadb to be ready
+sleep 5
+
+#only if wp not configured
 if [ ! -f "/var/www/html/index.html" ]; then
+	# dl wordpress 6.4
+	wp core download --allow-root
 	#edit a wp-config.php
 	wp config create	--allow-root --dbname=${SQL_DB_NAME} \
 				--dbuser=${SQL_USER_NAME} \
@@ -24,8 +29,6 @@ if [ ! -f "/var/www/html/index.html" ]; then
 				--user_pass=${USER_PASSWORD};
 	#on vide le cache
 	wp cache flush --allow-root
-	# set to english --> mostprob not needed
-	#wp language core install en_US --activate
 	# set the permalink structure (links with nice titles)
 	wp rewrite structure '/%postname%/'
 
@@ -33,8 +36,6 @@ fi
 
 #create a reboot persistent repo to save sockets files needed for phpfpm to communicate
 [[ ! -d /run/php ]] && mkdir /run/php
-
-alias php=/usr/bin/php82
 
 # start the PHP-FPM, in foreground
 /usr/sbin/php-fpm82 -F -R
