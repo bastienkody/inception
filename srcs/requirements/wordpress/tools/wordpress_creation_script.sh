ea26d295537d43e60383bfd54256b7982b845c03
kody <you@example.com>
2024-03-14 19:22:14 +0100

    little change wp

--- srcs/requirements/wordpress/tools/wordpress_creation_script.sh
+++ srcs/requirements/wordpress/tools/wordpress_creation_script.sh
@@ -3,30 +3,30 @@
 cd /var/www/html/wordpress
 
 # for mariadb to be ready
-sleep 5
+sleep 10
 
 #only if wp not configured
 if [ ! -f "/var/www/html/index.html" ]; then
 	# dl wordpress 6.4
 	wp core download --allow-root
 	#edit a wp-config.php
-	wp config create	--allow-root --dbname=${SQL_DB_NAME} \
+	wp config create --allow-root \
+				--dbname=${SQL_DB_NAME} \
 				--dbuser=${SQL_USER_NAME} \
 				--dbpass=${SQL_USER_PASSWORD} \
 				--dbhost=${SQL_HOST} \
-				--url=https://${DOMAIN_NAME};
+				--path='/var/www/wordpress'
 	#proper wp instalaltion
-	wp core install		--allow-root \
-				--url=https://${DOMAIN_NAME} \
+	wp core install --allow-root \
 				--title=${SITE_TITLE} \
 				--admin_user=${ADMIN_USER} \
 				--admin_password=${ADMIN_PASSWORD} \
-				--admin_email=${ADMIN_EMAIL};
+				--admin_email=${ADMIN_EMAIL}
 	#user of wp that can edit blogs
-	wp user create		--allow-root \
+	wp user create --allow-root \
 				${USER_LOGIN} ${USER_MAIL} \
 				--role=author \
-				--user_pass=${USER_PASSWORD};
+				--user_pass=${USER_PASSWORD}
 	#on vide le cache
 	wp cache flush --allow-root
 	# set the permalink structure (links with nice titles)
