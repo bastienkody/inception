021ef6a4977892c80180bf83fb8aa0d0c80462e2
bguillau <bguillau@student.42.fr>
2024-03-21 12:14:13 +0000

    on the way to nickel

--- srcs/requirements/mariadb/tools/mariadb_install_script.sh
+++ srcs/requirements/mariadb/tools/mariadb_install_script.sh
@@ -18,25 +18,24 @@ rc-update add mariadb default
 # launch it
 rc-service mariadb start
 
+sleep 1
+
 # db + user param
-#if [ ! -d "/var/lib/mysql/mysql" ]; then
+is_db_installed=$(mysql -u root -e "FLUSH PRIVILEGES;" > /dev/null 2>&1 ; echo $?) 
+if [ $is_db_installed == 0 ] ; then
 	echo "inside if statement mariadb"
 	echo "create db"
 	mysql -e "CREATE DATABASE IF NOT EXISTS ${SQL_DB_NAME};"
 	mysql -e "DROP USER 'mysql'@'localhost';"
 	echo "create user"
 	mysql -e "CREATE USER IF NOT EXISTS '${SQL_USER_NAME}'@'%' IDENTIFIED BY '${SQL_USER_PASSWORD}';"
-#	mysql  -e "ALTER USER 'mysql'@'localhost' IDENTIFIED BY '${SQL_USER_PASSWORD}';"
-#	mysql  -e "UPDATE mysql.user SET Host='%' WHERE Host='localhost' AND User='${SQL_USER_NAME}';"
 	mysql  -e "UPDATE mysql.db SET Host='%' WHERE Host='localhost' AND User='${SQL_USER_NAME}';"
 	mysql  -e "GRANT ALL PRIVILEGES ON ${SQL_DB_NAME}.* TO '${SQL_USER_NAME}'@'%' IDENTIFIED BY '${SQL_USER_PASSWORD}';"
 	echo "alter root"
 	mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${SQL_ROOT_PASSWORD}';"
 	echo "flush"
 	mysql -u root --password=${SQL_ROOT_PASSWORD} -e "FLUSH PRIVILEGES;"
-#fi
-
-sleep 2
+fi
 
 # launch daemon
 #rc-service mariadb restart --> it seems to daemonize but the commands exits and so the docker. With mysqld it does not exits. 
--- srcs/requirements/wordpress/tools/wordpress_creation_script.sh
+++ srcs/requirements/wordpress/tools/wordpress_creation_script.sh
@@ -2,45 +2,46 @@
 
 cd /var/www/html/wordpress
 
-# for mariadb to be ready
-sleep 10
+# 	for mariadb to be ready
+sleep 2
 
-#only if wp not configured
+#	only if wp not configured
 if [ ! -f "/var/www/html/wordpress/wp-config.php" ]; then
-	# dl wordpress 6.4
+	#	dl wordpress 6.4
 	wp core download --allow-root
-	#edit a wp-config.php
+	#	edit a wp-config.php
 	wp config create --allow-root \
 				--dbname=${SQL_DB_NAME} \
 				--dbuser=${SQL_USER_NAME} \
 				--dbpass=${SQL_USER_PASSWORD} \
 				--dbhost=${SQL_HOST} \
 				--url=${DOMAIN_NAME}
-	#proper wp instalaltion
+	#	proper wp instalaltion
 	wp core install --allow-root \
 				--title=${SITE_TITLE} \
 				--url=${DOMAIN_NAME} \
 				--admin_user=${ADMIN_USER} \
 				--admin_password=${ADMIN_PASSWORD} \
 				--admin_email=${ADMIN_EMAIL}
-	#user of wp that can edit blogs
+	#	user of wp that can edit blogs
 	wp user create --allow-root \
 				${USER_LOGIN} ${USER_MAIL} \
 				--role=author \
 				--user_pass=${USER_PASSWORD}
-	#on vide le cache
-	wp cache flush --allow-root
-	# set the permalink structure (links with nice titles) -> tant pis !
+	#	set the permalink structure (links with nice titles) 
+	#	leads to a problem for php-fpm to retrieve the posts/pages files -> tant pis !
 	#wp rewrite structure '/%postname%/' --allow-root
 
 fi
 
-#create a reboot persistent repo to save sockets files needed for phpfpm to communicate
+#	on vide le cache (dans tous les cas, ca fait pas de mal i guess?)
+wp cache flush --allow-root
+
+#	create a reboot persistent repo to save sockets files needed for phpfpm to communicate
 [[ ! -d /run/php ]] && mkdir /run/php
 
-# needed now bc does not exist in dockefile
+#	needed now bc does not exist in dockefile
 chown -R www-data:www-data /var/www/html/wordpress
 
-
-# start the PHP-FPM, in foreground
+#	restart the PHP-FPM, in foreground
 /usr/sbin/php-fpm82 -F -R
